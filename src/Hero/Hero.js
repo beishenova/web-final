c1de9447b9e020412e5efb9f51de19a158fe2852
Aidar <achakiev2003@gmail.com>
2022-01-06 20:41:03 +0600

    navbar & hidden sidebar

---
+++ src/Hero/Hero.js
@@ -0,0 +1,20 @@
+import { css } from "@emotion/core";
+import HeroNav from "./HeroNav/HeroNav";
+import Output from "../Output/Output";
+
+const Hero = () => {
+  return (
+    <section css={styles} className="hero">
+      <HeroNav />
+      <Output />
+    </section>
+  );
+};
+
+const styles = css`
+  width: 100%;
+  min-height: 100vh;
+  background: #1b1c22;
+`;
+
+export default Hero;
---
+++ src/Hero/Navbar/HeroNavLogo.js
@@ -0,0 +1,12 @@
+import { css} from "@emotion/core";
+
+const HeroNavLogo = () => <h2 css={styles}>Movies</h2>;
+
+const styles = css`
+  font-size: 22px;
+  color: #cb6ad2;
+  font-weight: 900;
+  user-select: none;
+`;
+
+export default HeroNavLogo;
