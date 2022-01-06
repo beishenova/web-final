ea12f77d4a01971bfe355274d86e94ea88f232f8
Aidar <achakiev2003@gmail.com>
2022-01-06 22:19:47 +0600

    fix

---
+++ src/Components/Output/Output.js
@@ -0,0 +1,47 @@
+import { css } from "@emotion/core";
+import React, { useContext } from "react";
+import { MovieContext } from "../../contexts/MovieContext";
+import Container from "../Container";
+import Movies from "../Movies/Movies";
+import MoviesPagination from "../Movies/MoviesPagination";
+import Popular from '../Popular'
+
+const Output = () => {
+  const { activeLink } = useContext(MovieContext);
+
+  return (
+    <div css={styles} className="output">
+      {activeLink === "All Movies" && (
+        <Container>
+          <React.Fragment>
+            <Movies />
+            <MoviesPagination />
+          </React.Fragment>
+        </Container>
+      )}
+      {activeLink === "Popular" && (
+        <Container>
+          <Popular />
+        </Container>
+      )}
+    </div>
+  );
+};
+
+const styles = css`
+  width: 100%;
+  min-height: calc(100vh - 80px);
+  display: flex;
+  > .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+  }
+  @media (max-width: 1365px) {
+    > .container {
+      max-width: 90%;
+    }
+  }
+`;
+
+export default Output;
